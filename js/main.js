ca0483cc6ad6960befa05905b5ddba9b7f4ffb6e
HeikkiMustonen <heikki.mustonen@student.oulu.fi>
2023-03-20 19:12:46 +0200

    video 3 stuff

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const menu = document.querySelector(".menu")
+const menuNav = document.querySelector(".menu-nav")
+const menuBtn = document.querySelector(".menu-btn")
+const menuBranding = document.querySelector(".menu-branding")
+
+const navItems = document.querySelectorAll(".nav-item")
+
+let showMenu = false;
+
+console.log("menu pressed!")
+menuBtn.addEventListener('click', toggleMenu)
+
+function toggleMenu() {
+    if(showMenu){
+        console.log("menu true")
+        menuBtn.classList.add('close')
+        menu.classList.add('show')
+        menuNav.classList.add('show')
+        menuBranding.classList.add('show')
+        navItems.forEach(item => {
+            item.classList.add('show')
+        })
+        
+        showMenu = false
+    }else{
+        console.log("menu false")
+        menuBtn.classList.remove('close')
+        menu.classList.remove('show')
+        menuNav.classList.remove('show')
+        menuBranding.classList.remove('show')
+        navItems.forEach(item => {
+            item.classList.remove('show')
+        })
+        showMenu = true
+    }
+}
